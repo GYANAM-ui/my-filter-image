b70d8650482abfe1d3fd4aaabcdf2e6498015f34
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-04-04 12:42:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-function preload() {
-
-  }
-  
-  function setup() {
-    canvas = createCanvas(300, 300);
+function preload()
+{
+    
+}
+function setup()
+{
+    canvas = createCanvas(300,300);
     canvas.center();
-  }
-  function draw() {
+    video = createCapture(VIDEO);
+    video.size(350,300);
+    video.hide();
+    color = " ";
 
-  }
-  
-  function take_snapshot(){    
-    save('myFilterImage.png');
-  }
+    PoseNet = ml5.poseNet(video,modelLoaded);
+    PoseNet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log('nose x = '+results[0].pose.nose.x);
+        console.log('nose y = '+results[0].pose.nose.y);
+    }
+}
+function draw()
+{
+    image(video,0,0,300,300);
+    tint(color);
+}
+function color_catch()
+{
+    color = document.getElementById("color").value;
+}
+function take_snapshot()
+{
+    save("myFilterImage.png");
+}
